5c77c89dfa84c477fb44d964c13e56f3d762c348
Totemy <thetotemofficial@gmail.com>
2026-02-05 17:46:00 +0200

    Connect Home view to main page

---
+++ frontend/shop/src/services/product.service.ts
@@ -0,0 +1,11 @@
+import { http } from '../api/http'
+
+export const ProductService = {
+    getAll() {
+        return http.get('/products')
+    },
+
+    getById(id: string) {
+        return http.get(`/products/${id}`)
+    },
+}
---
+++ frontend/shop/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { ProductService } from '../services/product.service'
+
+const products = ref([])
+
+onMounted(async () => {
+    const { data } = await ProductService.getAll()
+    products.value = data
+})
+</script>
+
+<template>
+    <div>
+        <h1>Products</h1>
+        <pre>{{ products }}</pre>
+    </div>
+</template>
